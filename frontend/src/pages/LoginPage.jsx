26335bf89c97824d980eeb984f555cab43934998
Dgprogramas <dicohele@gmail.com>
2025-09-13 17:47:00 -0300

    :lipstick:feat: user register interface styling
    Co-authored-by: Gustavo Rodrigues <g.srodrigues@outlook.com>

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react'; // <-- ADD THIS LINE
-import { Link } from 'react-router-dom';
-import ProfileCard from '../components/ProfileCard';
+import React from "react"; // <-- ADD THIS LINE
+import { Link } from "react-router-dom";
+import ProfileCard from "../components/ProfileCard";
 
 function HomePage() {
   return (
@@ -10,7 +10,7 @@ function HomePage() {
       <div className="absolute top-5 right-5 flex space-x-4">
         <Link to="/registrar">
           <button className="px-4 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition-colors">
-            Registrar
+            Registre-se
           </button>
         </Link>
         <Link to="/sobre">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rajdhani: ["Rajdhani", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        "64xl": "64px", // O tamanho personalizado para 64px
+        "160xl": "160px", // O tamanho personalizado para 160px
+      },
+    },
   },
-  plugins: [],
 };
-
