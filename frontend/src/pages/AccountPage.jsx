fad67be23f16d3032d65427f039f0277e5e2ae56
Dgprogramas <dicohele@gmail.com>
2025-09-24 20:42:26 -0300

    lipstick:feat: add icon in loading screen

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -135,7 +135,27 @@ const AccountPage = () => {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-        Carregando informações da conta...
+        {/* Spinner de carregamento */}
+        <svg
+          className="animate-spin h-10 w-10 text-black"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          />
+        </svg>
       </div>
     );
   }
